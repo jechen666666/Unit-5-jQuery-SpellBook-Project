948c618a2b9ac8883434995913cbe8903b005784
jechen666666 <43358343+jechen666666@users.noreply.github.com>
2019-01-14 16:54:09 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+//Spell #1
+
+$(".HIDE").click(function(){
+    $(".Bunny").hide();
+});
+
+
+
+//Spell #2
+
+$(".FADE").click(function(){
+    $(".Ink").fadeOut();
+ 
+});
+
+
+
+//Spell #3
+
+$(".SLIDE").click(function(){
+    $(".GarageDoor").slideUp();
+ 
+});
+
+
+
+
